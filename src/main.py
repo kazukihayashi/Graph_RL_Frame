eb80c71c7c9c0c7a431150c13c9415c3ba7e6376
Kazuki Hayashi <hayashi.kazuki.55a@gmail.com>
2024-01-13 00:36:11 +0900

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -3,18 +3,19 @@ import Environment
 import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-### User specified parameters ###
-USE_GPU = True
-N_EPISODE = 5000
-
-## 'inc' if increasing the size from the minimum cross-section,
-## 'dec' if decreasing the size from the maximum cross-section
-MODE = 'inc'
-#################################
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--mode", help="Specify inc if increasing the size from the minimum cross-sections; specify dec if reducing the size from the maximum cross-sections.", default="inc", type=str)
+parser.add_argument("--test_model", help="Structural model to test the trained model's performance", default=1, type=int)
+parser.add_argument("--train", help="True if implement the training. False if only using the pre-trained machine learning model.", default=False, type=bool)
+parser.add_argument("--use_gpu", help="Use GPU if True, use CPU if False", default=False, type=bool)
+parser.add_argument("--n_episode", help="Number of episodes to train the machine learning model.", default=5000, type=int)
+args = parser.parse_args()
 
 t1 = time.time()
-env = Environment.Environment(gpu=USE_GPU,mode=MODE)
-# env.Train(N_EPISODE) # Uncomment when training
+env = Environment.Environment(gpu=args.use_gpu,mode=args.mode)
+if args.train:
+    env.Train(args.n_episode)
 t2 = time.time()
 env.Test(test_model=1)
 print("time: {:.3f} seconds".format(t2-t1))
